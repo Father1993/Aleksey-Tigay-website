6aeedd31545aa5f570a31055f0831a89c910c8d9
Andrej Spinej <enjoyhillol@gmail.com>
2024-03-25 16:01:05 +1000

    add nextjs-SEO

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+import { DefaultSeo } from 'next-seo'
 import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import Join from './components/Join/Join'
@@ -6,10 +6,15 @@ import Plans from './components/Plans/Plans'
 import Programs from './components/Programs/Programs'
 import Reasons from './components/Reasons/Reasons'
 import Testimonials from './components/Testimonials/Testimonials'
+import './App.css'
 
 function App() {
     return (
         <div className="App">
+            <DefaultSeo
+                title="Алексей Тигай | Персональный тренер"
+                description="Алексей Тигай - профессиональный, персональный тренер в Хабаровск. Индивидуальные программы тренировок, питания и диета. Запишитесь на первую тренировку уже сегодня!. Фитнес в Хабаровске. МК-Спорт. Спортзал."
+            />
             <Hero />
             <Programs />
             <Reasons />
