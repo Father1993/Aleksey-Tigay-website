ea00990bd9dd2396b3660215086dca00838ad9a1
Andrej Spinej <enjoyhillol@gmail.com>
2024-03-25 16:31:12 +1000

    add SEO react-hemlet

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet'
 import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import Join from './components/Join/Join'
@@ -10,6 +11,26 @@ import './App.css'
 function App() {
     return (
         <div className="App">
+            <Helmet>
+                <title>Алексей Тигай | Персональный тренер</title>
+                <meta
+                    name="description"
+                    content="Алексей Тигай - профессиональный, персональный тренер в Хабаровск. Индивидуальные программы тренировок, питания и диета. Запишитесь на первую тренировку уже сегодня!. Фитнес в Хабаровске. МК-Спорт. Спортзал."
+                />
+                <meta
+                    name="keywords"
+                    content="Персональный тренер, Алексей Тигай, Хабаровск, Фитнес, МК-Спорт, Спортзал"
+                />
+                <meta
+                    property="og:title"
+                    content="Алексей Тигай | Персональный тренер"
+                />
+                <meta
+                    property="og:description"
+                    content="Алексей Тигай - профессиональный, персональный тренер в Хабаровск. Индивидуальные программы тренировок, питания и диета. Запишитесь на первую тренировку уже сегодня!. Фитнес в Хабаровске. МК-Спорт. Спортзал."
+                />
+                <meta property="og:image" content="./assets/logo.png" />
+            </Helmet>
             <Hero />
             <Programs />
             <Reasons />
